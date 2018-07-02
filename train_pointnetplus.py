30c5abdc8a9cd313c42db79b1fac0130eb7d226d
lyc <827498975@gmail>
2018-07-02 11:46:02 +0800

    减少测试时的显存占用

--- train_pointnetplus.py
+++ train_pointnetplus.py
@@ -5,9 +5,9 @@ from data.modelnet import ModelNetDataset
 trainset = ModelNetDataset('/opt/modelnet40_normal_resampled', train=True)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=8, shuffle=True, num_workers=2)
 testset = ModelNetDataset('/opt/modelnet40_normal_resampled', train=False)
-testloader = torch.utils.data.DataLoader(testset, batch_size=4, shuffle=True, num_workers=2)
+testloader = torch.utils.data.DataLoader(testset, batch_size=16, shuffle=True, num_workers=2)
 
 net = PointNetPlusClassify(trainset.class_nums)
-for epcho in range(1, 2):
+for epcho in range(1, 60):
     net.fit(trainloader, epcho)
 net.score(testloader)
